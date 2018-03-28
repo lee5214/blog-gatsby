7fe61f6918737e2d10a86e9d3473a9b2e6989406
congli <admin@lee5214.com>
2018-03-28 02:03:03 -0700

    contact 6

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,17 +5,6 @@ const About = () => {
 	return (
 		<div>
 			<MessageWall/>
-			<form
-				name="contact-form"
-				method="post"
-				data-netlify="true"
-				data-netlify-honeypot="bot-field"
-			>
-				<input name="name" placeholder="Your Name" type="text"/>
-				<input name="email" placeholder="name@name.com" type="email"/>
-				<textarea name="message"/>
-				<button>Send</button>
-			</form>
 		</div>
 	);
 };
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,53 +3,21 @@ import { TextField } from 'material-ui';
 
 const Contact = () => {
 	return (
-		<div style={{display:'flex',justifyContent:'center'}}>
+		<div>
+			<h1>Hi people</h1>
+			<p>Welcome to your new Gatsby site.</p>
+			<p>Now go build something great.</p>
 			<form
-				name="contact"
+				name="contact-form"
 				method="post"
 				data-netlify="true"
 				data-netlify-honeypot="bot-field"
 			>
-					<TextField
-						id="with-placeholder"
-						name={'name'}
-						label='Name'
-						placeholder="John Snow"
-						type="text"
-						required
-						inputProps={{
-
-						}}
-					/>
-				{/*<div>
-					<TextField
-						id="with-placeholder"
-						name={'email'}
-						label='Email'
-						placeholder="xxxx@xxxx.com"
-						type="text"
-						required
-					/>
-				</div>
-				<div>
-					<TextField
-						id="multiline-static"
-						name={'message'}
-						label="Multiline"
-						multiline
-						rows="4"
-						margin="normal"
-						required
-					/>
-				</div>*/}
-
-				{ /*<input name="email" placeholder="king@google.com" type="email"/>*/ }
-
-				{/*<textarea name="message"/>*/}
+				<input name="name" placeholder="Your Name" type="text" />
+				<input name="email" placeholder="name@name.com" type="email" />
+				<textarea name="message" />
 				<button>Send</button>
 			</form>
-
-
 		</div>
 	);
 };
