7d824a39d1201bc513532a593f8534bc502c56c2
congli <admin@lee5214.com>
2018-03-28 01:36:03 -0700

    contact 5

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import MessageWall from '../components/Wall/MessageWall'
+import MessageWall from '../components/Wall/MessageWall';
+
 const About = () => {
 	return (
-		<MessageWall />
+		<div>
+			<MessageWall/>
+			<form
+				name="contact-form"
+				method="post"
+				data-netlify="true"
+				data-netlify-honeypot="bot-field"
+			>
+				<input name="name" placeholder="Your Name" type="text"/>
+				<input name="email" placeholder="name@name.com" type="email"/>
+				<textarea name="message"/>
+				<button>Send</button>
+			</form>
+		</div>
 	);
 };
 export default About;
