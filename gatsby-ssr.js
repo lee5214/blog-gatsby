ea121d1843791a3d3eeb6446a24d7f6aa135e181
congli <admin@lee5214.com>
2018-07-18 07:55:58 -0700

    try ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { JssProvider } from 'react-jss';
@@ -34,3 +35,4 @@ exports.replaceRenderer = ({
     />,
   ]);
 };
+*/
