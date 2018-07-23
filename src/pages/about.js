74839bf4381a35c35a33b992537fded3d5f7470b
congli <admin@lee5214.com>
2018-07-23 01:16:29 -0700

    design update, add wave, change styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ class About extends Component {
   render() {
     return (
       <div style={{color:'white'}}>
-        LinkedIn : <a href={'https://linkedin.com/in/lee5214.com'}>Lee5214</a>
+        LinkedIn : <a href={'https://linkedin.com/in/lee5214'}>Lee5214</a>
       </div>
     );
   }
